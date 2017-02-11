6d7748000eca4760bef05855e6d1bfa6cb3319c5
faturita <rramele@gmail.com>
2017-02-11 13:16:12 -0300

    Added dynamic time warping and improved artifact recognition

--- isartifact.m
+++ isartifact.m
@@ -1,7 +1,12 @@
-%% If Signal(t,c) is greater than 70, it is considered artifact.
+%% If Signal(t,c) is greater than 70, on any channel, it is considered artifact.
 function ret = isartifact(signal)
 
-iff = ((abs(signal)>70));
+output = signal;
+
+[n,m]=size(output);
+output=output - ones(n,1)*mean(output,1);
+              
+iff = ((abs(output)>70));
 ifs = find(sum(iff'>0));
 
 ret = ~isempty(ifs);
