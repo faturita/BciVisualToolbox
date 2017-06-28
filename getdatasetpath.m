7a68daedb7abec9a7db1b7cc009c826a2d8febcf
faturita <rramele@gmail.com>
2017-06-28 14:50:13 -0300

    Update datasetpath for server

--- getdatasetpath.m
+++ getdatasetpath.m
@@ -2,9 +2,12 @@ function basepath=getdatasetpath()
 
 basepath = 'C:/Users/rramele/Google Drive/BCI.Dataset';
 if exist(basepath,'dir')==0
-    basepath = 'C:/Users/User/Google Drive/BCI.Dataset';
+    basepath = 'E:/Google Drive/BCI.Dataset';
     if exist(basepath,'dir')==0
         basepath = sprintf('%s/GoogleDrive/BCI.Dataset',getenv('HOME'));
+        if (exist(basepath,'dir')==0)
+            basepath = sprintf('%s/GoogleDrive/BCI.Dataset',getenv('HOMEPATH'));
+        end
     end
 end
 
