e86e8c28bf0d64bdbec9f9f357e3417d6c6ac50f
faturita <rramele@gmail.com>
2018-10-12 20:01:29 -0300

    Add some parameter to drawfft to allow adding a 50 hz signal

--- drawfft.m
+++ drawfft.m
@@ -1,4 +1,4 @@
-function powerspectraldensity = drawfft(signal, drawme, Fs)
+function powerspectraldensity = drawfft(signal, drawme, Fs,addfiftyhertz)
 % PSD = drawfft(signal, darwme, Fs)
 %
 % signal is output(t,channel)'
@@ -7,13 +7,21 @@ if (nargin<3)
     Fs = 128;                    % Sampling frequency (EPOC frequency)
 end
 
+if (nargin<4)
+    addfiftyhertz=false;
+end
+
 T = 1/Fs;                     % Sample time
 L = size(signal,2);                     % Length of signal
 t = (0:L-1)*T;                % Time vector
 
 % I add a 50 Hz TRASH signal to understand if the FFT is working.
-%x1 = 70.7*sin(2*pi*50*t) + signal ;
-x1 = signal;
+
+if (addfiftyhertz)
+    x1 = 0.5*sin(2*pi*50*t) + signal ;
+else
+    x1 = signal;
+end
 
 %h = fdesign.bandpass('Fp,Fst,Ap,Ast',9,11,0.5,70,128);
 
@@ -32,9 +40,11 @@ if (drawme == 1)
     plot(f,2*abs(Y(1:NFFT/2+1))) ;
     %axis([6 14 -0.2 2.5]);
     %axis([0 60 -0.2 0.6]);
-    axis([0 60 -0.2 0.5]);
-    xlabel('Frequency (Hz)');
-    ylabel('|Y(f)|');
+    %axis([0 60 -0.2 0.5]);
+    hy = ylabel('|Y(f)|');
+    hx = xlabel('Frequency (Hz)');
+    set(hx,'fontSize',20);
+    set(hy,'fontSize',20);
 end
 
 powerspectraldensity = mean ( ( 2*abs(Y(1:NFFT/2+1)) ) ); 
